630e493a0bc689a76ffd57db8fe7d5893fd6ef5f
Showaiz <84308802+Showaiz@users.noreply.github.com>
2021-11-16 13:00:23 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-let height = randint(0, 100)
-if (height > 5 && height < 20) {
+let height = 5
+if (height >= 10 && height <= 20) {
     scene.setBackgroundColor(7)
 } else {
     scene.setBackgroundColor(5)
